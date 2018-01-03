7914c0e7bedf760bdec542dc1a0d1b605d220c4e
Hoa Bui <hoabuisla123@gmail.com>
2018-01-03 08:30:54 +0700

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_page/Home'
+
+  get 'static_page/Help'
+
   root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
